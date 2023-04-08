b063c962d77eab7efc0df6c66506116f6a77e85c
AndrzejSmrokowski <fireacepl@gmail.com>
2023-04-08 17:24:25 +0200

    added new test

--- src/main/java/com/junioroffers/domain/offer/dto/OfferRequestDto.java
+++ src/main/java/com/junioroffers/domain/offer/dto/OfferRequestDto.java
@@ -2,8 +2,8 @@ package com.junioroffers.domain.offer.dto;
 
 public record OfferRequestDto(
         String companyName,
+        String position,
         String salary,
-        String url,
-        String position
+        String url
 ) {
 }
--- src/test/java/com/junioroffers/domain/offer/OfferFacadeTest.java
+++ src/test/java/com/junioroffers/domain/offer/OfferFacadeTest.java
@@ -1,5 +1,6 @@
 package com.junioroffers.domain.offer;
 
+import com.junioroffers.domain.offer.dto.JobOfferResponse;
 import com.junioroffers.domain.offer.dto.OfferRequestDto;
 import com.junioroffers.domain.offer.dto.OfferResponseDto;
 import org.junit.jupiter.api.Test;
@@ -30,5 +31,32 @@ class OfferFacadeTest {
         List<OfferResponseDto> result = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
         //then
         assertThat(result).hasSize(6);
+    }
+    @Test
+    void shouldSaveOnly2OffersWhenRepositoryHad4AddedWithUrls() {
+        //given
+        OfferFacade offerFacade = new OfferFacadeTestConfiguration(
+                List.of(
+                        new JobOfferResponse("id", "id", "asds", "1"),
+                        new JobOfferResponse("assd", "id", "asds", "2"),
+                        new JobOfferResponse("asddd", "id", "asds", "3"),
+                        new JobOfferResponse("asfd", "id", "asds", "4"),
+                        new JobOfferResponse("Junior", "Comarch", "1000", "https://someurl.pl/5"),
+                        new JobOfferResponse("Mid", "Finanteq", "2000", "https://someother.pl/6")
+                )
+        ).offerFacadeForTests();
+        offerFacade.saveOffer(new OfferRequestDto("id", "position1", "500", "1"));
+        offerFacade.saveOffer(new OfferRequestDto("id", "position2", "300", "2"));
+        offerFacade.saveOffer(new OfferRequestDto("id", "position3", "200", "3"));
+        offerFacade.saveOffer(new OfferRequestDto("id", "position4", "100", "4"));
+        assertThat(offerFacade.findAllOffers()).hasSize(4);
+        //when
+        List<OfferResponseDto> response = offerFacade.fetchAllOffersAndSaveAllIfNotExists();
+        //then
+        assertThat(List.of(
+                response.get(0).url(),
+                response.get(1).url()
+        )).containsExactlyInAnyOrder("https://someurl.pl/5", "https://someother.pl/6");
+
     }
 }
